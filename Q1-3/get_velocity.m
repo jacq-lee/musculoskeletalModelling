018ed599bad11acadc2559d55e77003da627b8db
Mara Jenkins <109381857+mara-j@users.noreply.github.com>
2023-03-08 14:12:41 -0500

    Update get_velocity.m

--- Q1-3/get_velocity.m
+++ Q1-3/get_velocity.m
@@ -8,7 +8,7 @@ function [root] = get_velocity(a, lm, lt)
 % Output
 % root: normalized lengthening velocity of muscle (contractile element)
 
-x = fzero(fun,x0)   % tries to find a point x where fun(x) = 0. This solution is where fun(x) changes sign—fzero cannot find a root of a function such as x^2.
+root = fzero(fun,x0)   % tries to find a point x where fun(x) = 0. This solution is where fun(x) changes sign—fzero cannot find a root of a function such as x^2.
 
 
 % damping coefficient (see damped model in Millard et al.)
