d81ccf6854a7784c215e9ef9bec2a74bb676c562
Jacqueline L <o.jacqueline.lee@gmail.com>
2023-03-10 10:03:18 -0500

    Finish Q3

--- Q1-3/simulate.m
+++ Q1-3/simulate.m
@@ -11,13 +11,13 @@ muscle_model = HillTypeMuscle;
 muscle_model.resting_length_muscle = resting_length_muscle;
 muscle_model.resting_length_tendon = resting_length_tendon;
 muscle_model.f0M = f0M;
+muscle_tendon_total_length = resting_length_muscle + resting_length_tendon;
 
 %%% TASK 2
 
     function [vm] = nested_velocity(time, norm_lm)
 
-        muscle_tendon_length = resting_length_muscle + resting_length_tendon;
-        norm_lt = norm_tendon_length(muscle_model, muscle_tendon_length, norm_lm);
+        norm_lt = norm_tendon_length(muscle_model, muscle_tendon_total_length, norm_lm);
         
         if time < 0.5
                 a = 0;
@@ -42,6 +42,12 @@ tspan = [t0 tf];
 
 %%% TASK 4
 % save the estimated forces in a vector called "forces"
+
+forces = zeros(1,length(norm_lm));
+
+for n = 1:1:length(norm_lm)
+    forces(n) = get_force(muscle_model, muscle_tendon_total_length, norm_lm(n));
+end
 
 %%%% Do not alter the rest (it should not be needed) %%%%%%
 norm_lm = norm_lm * resting_length_muscle;
